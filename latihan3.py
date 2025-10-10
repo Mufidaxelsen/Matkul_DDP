bd2b71bebb0e837e92039d4eec05ac682060fde3
Zack queen <mhmmadd.mufid@gmail.com>
2025-10-10 09:19:43 +0700

    Matkul DDP

---
+++ latihan3.py
@@ -0,0 +1,14 @@
+# latihan IF
+pelanggan = "Mufid"
+totalBelanja = 150000
+
+# kondisional IF
+if totalBelanja > 100000:
+    keterangan = "Dapat Diskon Shoope 11.11"
+else:
+    keterangan = "Terimakasih telah menghabiskan uangnya"
+
+# Mencetak
+print (pelanggan, "Orderan Berhasil",
+       "\nTotal Belanja Anda\t:", totalBelanja,
+       "\nKeterangan\t\t:", keterangan)
---
+++ latihan5.py
@@ -0,0 +1,18 @@
+# latihan if multi kondisi
+
+nama = "Muhammad Mufid Azhar"
+jab = "Staff"
+if jab == "Manager" :
+    gaji = 1500000
+elif jab == "Asmen" :
+    gaji = 2000000
+elif jab == "Staff" :
+    gaji = 2000000
+elif jab == "Office" :
+    gaji = 2000000
+else:
+    gaji = 0
+
+print ("Orderan Berhasil\t", nama,
+       "\nJabatan\t:", jab,
+       "\nGaji RP.\t\t:", gaji)
